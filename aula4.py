0c2c9bc88916d6fdbb413fadd590968aaf94a903
Fabio Martins <fabio@fastmail.uk>
2019-06-25 13:56:53 -0300

    Versão 1 - Criada dia 25/06

--- aula4.py
+++ aula4.py
@@ -1,4 +1,7 @@
 lista_nomes = ['Joao', 'Claudio', 'Fabio', 'Diego']
+lista_nomes.append("Geralda")
+lista_nomes.remove('Fabio')
+lista_nomes.insert(0,'Jose')
 
 print(type(lista_nomes))
-print(lista_nomes[0:13:])
+print(lista_nomes)
