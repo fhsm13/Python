ee95fe1d041d634757cf401be64a0387afc68fca
Fabio Martins <fabio@fastmail.uk>
2019-06-25 15:14:01 -0300

    Versão 1 - Criada dia 25/06 Exercicio adicional

--- aula4.py
+++ aula4.py
@@ -2,6 +2,10 @@ lista_nomes = ['Joao', 'Claudio', 'Fabio', 'Diego']
 lista_nomes.append("Geralda")
 lista_nomes.remove('Fabio')
 lista_nomes.insert(0,'Jose')
+contador = lista_nomes.count('Joao')
 
+print(lista_nomes.pop())
+print(len(lista_nomes))
+print(contador)
 print(type(lista_nomes))
 print(lista_nomes)
