183ee5e95bb3e28b86b9b5132984842bf83eb5ab
Fabio Martins <fabio@fastmail.uk>
2019-06-25 21:06:14 -0300

    Versão 1 - Criada dia 25/06 Exercicio adicional

--- functions.py
+++ functions.py
@@ -3,8 +3,12 @@ dias_mes = input('Digite quantidade de dias no mês: ')
 vlr_horas = float(vlr_horas)
 dias_mes = float(dias_mes)
 
-salario = vlr_horas * dias_mes
-print("Salario: {}" .format(salario))
+if vlr_horas > 0:
+    salario = vlr_horas * dias_mes
+    print("Salario: {}".format(salario))
+else:
+    print("Digitou 0")
+
 
 
 
