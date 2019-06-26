46fafe62d76701d9c58c49d1a41d2b8689b7eea4
Fabio Martins <fabio@fastmail.uk>
2019-06-25 21:09:05 -0300

    Versão 1 - Criada dia 25/06 Exercicio adicional

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
+'''
+Autor: Fabio Martins
+Email: fabio@fastmail.uk
+Format se utiliza para formatar o tipo de dado inserido para cálculo posterior
+sem este tipo de atributo nâo é possível efetuar o cálculo abaixo, sendo necessário formatar para que nâo ocorra erro
+Também foi declarado o tipo de variável para inserção dos dados veja que o tipo float
+'''
+
 vlr_horas = input('Digite as hora VALOR: ')
 dias_mes = input('Digite quantidade de dias no mês: ')
 vlr_horas = float(vlr_horas)
